44b2efe40eab5a6b61186730922edaaaf8e81e23
Manvendra Singh <manvendrasingh2260@gmail.com>
2026-04-29 14:52:00 +0530

    port changed

--- lec-5/app.js
+++ lec-5/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const handler = require('./one');
 const server = http.createServer(handler);
-const port = 3000;
+const port = 3001;
 server.listen(port,()=>{
-  console.log(`server is runnig on http://localhost:3000`);
+  console.log(`server is runnig on http://localhost:3001`);
 });
